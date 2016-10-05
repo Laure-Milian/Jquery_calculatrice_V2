f7d80607f6deb0bf2ab0b5a5aba3e66d26706132
Laure <laure.milian@gmail.com>
2016-10-05 12:25:41 +0200

    Calculatrice avec touches - à refactoriser

--- CalcV2nombres.js
+++ CalcV2nombres.js
@@ -15,9 +15,34 @@
 		}
 	};
 
+	var objKeysNumbers = {
+		key48 : 0,
+		key49 : 1,
+		key50 : 2,
+		key51 : 3,
+		key52 : 4,
+		key53 : 5,
+		key54 : 6,
+		key55 : 7,
+		key56 : 8,
+		key57 : 9
+	};
 
-	$(".number").click(function() {
-		var number = $(this).data("type");
+	var objKeysOperateurs = {
+		key42 : "*",
+		key43 : "+",
+		key45 : "-",
+		key47 : "/"
+	};
+
+	$("html").on("keypress", function(e) {
+		var operateur = objKeysOperateurs["key" + e.keyCode];
+		$("#operation").html(operateur);
+	});
+
+
+	$("html").on("keypress", function(e) {
+		var number = objKeysNumbers["key" + e.keyCode];
 		var operateur = $("#operation").text();
 
 		if (!operateur) {
@@ -28,51 +53,90 @@
 		}
 	});
 
+	$("html").on("keypress", function(e) {
+		if (e.keyCode === 13) {
+			var nb1 = parseInt(($("#nb1").text()), 10);
+			var nb2 = parseInt(($("#nb2").text()), 10);
+			var operateur = $("#operation").text();
 
-	$(".operateur").click(function() {
-		var operateur = $(this).data("type");
-		$("#operation").html(operateur);
+			if (operateur === "+") {
+				var resultat = Calculer["add"](nb1, nb2);
+			}
+
+			else if (operateur === "-") {
+				var resultat = Calculer["substract"](nb1, nb2);
+			}
+
+			else if (operateur === "*") {
+				var resultat = Calculer["multiply"](nb1, nb2);
+			}
+
+			else if (operateur === "/") {
+				var resultat = Calculer["divide"](nb1, nb2);
+			}
+
+			$("#result").html(resultat);
+
+		}
 	});
 
 
-	$("#calculer").click(function calc() {
+//au clic 
+$(".number").click(function() {
+	var number = $(this).data("type");
+	var operateur = $("#operation").text();
 
-		var nb1 = parseInt(($("#nb1").text()), 10);
-		var nb2 = parseInt(($("#nb2").text()), 10);
-		var operateur = $("#operation").text();
+	if (!operateur) {
+		$("#nb1").append(number);
+	}
+	else {
+		$("#nb2").append(number);
+	}
+});
 
-		if (operateur === "+") {
-			var resultat = Calculer["add"](nb1, nb2);
-		}
 
-		else if (operateur === "-") {
-			var resultat = Calculer["substract"](nb1, nb2);
-		}
+$(".operateur").click(function() {
+	var operateur = $(this).data("type");
+	$("#operation").html(operateur);
+});
 
-		else if (operateur === "*") {
-			var resultat = Calculer["multiply"](nb1, nb2);
-		}
 
-		else if (operateur === "/") {
-			var resultat = Calculer["divide"](nb1, nb2);
-		}
+$("#calculer").click(function calc() {
 
-		$("#result").html(resultat);
+	var nb1 = parseInt(($("#nb1").text()), 10);
+	var nb2 = parseInt(($("#nb2").text()), 10);
+	var operateur = $("#operation").text();
 
-	});
+	if (operateur === "+") {
+		var resultat = Calculer["add"](nb1, nb2);
+	}
 
-	$("#reinitialiser").on("click", function() { 
-		$("#nb1").text("");
-		$("#nb2").text("");
-		$("#operation").text("");
-		$("#result").text("");
-	});
-	
+	else if (operateur === "-") {
+		var resultat = Calculer["substract"](nb1, nb2);
+	}
 
-/*var KEY_1 = 49; ou objet keys.
+	else if (operateur === "*") {
+		var resultat = Calculer["multiply"](nb1, nb2);
+	}
 
-	$("").on("keypress", function(e) {
-		console.log(e.keyCode);
-	})*/
+	else if (operateur === "/") {
+		var resultat = Calculer["divide"](nb1, nb2);
+	}
+
+	$("#result").html(resultat);
+
+});
+
+$("#reinitialiser").on("click", function() { 
+	$("#nb1").text("");
+	$("#nb2").text("");
+	$("#operation").text("");
+	$("#result").text("");
+});
+
+//fin au clic
+
+
+
 
 })();
